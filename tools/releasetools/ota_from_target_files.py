2ab58b275b28497cc3ed8d9361076b4883f956a6
trautamaki <timi.rautamaki@gmail.com>
2020-07-22 20:43:22 +0300

    build: ota: fix typo
    Change-Id: Ided5bcfe62fb185d0e15b54da37be0d3ad66eb82

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -1057,7 +1057,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   if OPTIONS.backuptool:
     script.ShowProgress(0.02, 10)
     script.RunBackup("restore", sysmount)
-    script.Print("Running backuptool: restore);
+    script.Print("Running backuptool: restore");
 
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
